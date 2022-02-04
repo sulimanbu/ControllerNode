b1342b7136f60370236a739ebcc0a777ec57f05d
Suliman <SulimanAlburmawy998@gmail.com>
2022-02-04 23:04:49 +0200

    Add Logger

--- src/main/java/com/example/controllernode/ControllerNodeApplication.java
+++ src/main/java/com/example/controllernode/ControllerNodeApplication.java
@@ -2,17 +2,22 @@ package com.example.controllernode;
 
 import com.example.controllernode.Services.Helper.IdGenerator;
 import com.example.controllernode.Services.IServices.IUserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class ControllerNodeApplication {
 
     final IUserService userService;
+    @Value("${spring.application.Data_Base_Path}")
+    String Data_Base_Path;
     public ControllerNodeApplication(IUserService userService) {
         this.userService = userService;
     }
@@ -23,8 +28,8 @@ public class ControllerNodeApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void doSomethingAfterStartup() throws IOException {
+        Files.createDirectories(Path.of(Data_Base_Path));
         userService.addFirstUser();
         IdGenerator.getOldIds();
-
     }
 }
